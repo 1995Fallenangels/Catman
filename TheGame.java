b5300ae373174c8be01e7d4de4ed9c506d38d2c6
GABRIELLAA <134564841+1995Fallenangels@users.noreply.github.com>
2024-05-24 10:46:21 +1200

    continued w the maze but there's a bug
    Added some System.out.println lines to help me debug so ignore those

--- TheGame.java
+++ TheGame.java
@@ -12,7 +12,7 @@
  * 4) a healthbar. This will show how many lives the kitten has left. The cat will have nine lives represented as hearts (because cats have 9 lives).
  * When the cat runs into a dog, the cat will lose 3 lives.
  * @author gabriella
- * @version 23/05
+ * @version 24/05
  */
 import javax.swing.*;
 import java.awt.*;
@@ -20,9 +20,9 @@ public class TheGame extends JFrame
 {
     int colomns = 28;//this is setting the number of cell colomns in the maze to 21.
     int rows = 36; //sets the cell rows in the maze to 18.
-    //int border = 20; //how far the maze is away from the edge of the panel.
+    int topBorder = 3; //how far the maze is away from the edge of the panel.
     int mazeRows=31;
-    int mazeColomns=28;
+    int mazeColumns=28;
     int cellSize = 25;// this sets the size of one cell to 25 pixels. 
     private int[][] maze;// this makes an array which the values of maze[i][j] can hold a value of being a wall or a road that the cat and dogs can walk through.
     //the road has little white dots/pellets the cat can eat. When the cat eats the pellet the road become empty.
@@ -36,18 +36,21 @@ public class TheGame extends JFrame
      */
     public TheGame(){
         JFrame window = new JFrame("Catman");
-        //window.setPreferredSize(new Dimension(((cellSize*colomns)+(cellSize*border)),((cellSize*rows)+(cellSize*border))));//sets the size of the window
+        window.setPreferredSize(new Dimension((cellSize*colomns),(cellSize*rows)));//sets the size of the window
         window.getContentPane().setBackground(new Color(255,238,238));// I made the background colour baby pink. This correlates to my relevant implications of aesthetics
         setLocationRelativeTo(null);//this is meant to set the window to the middle of the computer screen.
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);//makes the window close when it needs to
         window.pack();//this sizes the window to the preffered size
         window.toFront();//makes the game the focused window.
         window.setVisible(true);//makes it visible
-        //this.maze = makeMaze();
+        System.out.println("hi");
+        repaint();
     }
-    public void makeMaze(){
+    public void makeMaze(Graphics g){
         //these are the values of the array that makes up the maze. 0's are walls, 1's are roads with dots,
         //2's are pellets, 3's are empty roads 4's are empty spaces that the cat can't go in. (The centre, where all the 4's are, is where the dogs will appear. The cat can't go there.)      
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
         int maze[][] = 
             {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
              {0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
@@ -81,6 +84,71 @@ public class TheGame extends JFrame
              {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
              {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
         //now i need to print the array
-        for(int i = 0; i < 28; i++){}
+        for(int i = 0; i < mazeRows; i++){
+            for(int j = 0; j<mazeColumns; j++){
+                System.out.print(maze[j][i]);
+                switch (maze[j][i]){
+                    case 0:
+                        g2.setColor(Color.BLACK);
+                        g2.fillRect(i*cellSize,j*cellSize, cellSize, cellSize);
+                        System.out.print("hi");
+                    case 1:
+                        g2.setColor(new Color(255,238,238));
+                        break;
+                }    
+                }
+            }
+        }
+    public void paint(Graphics g){
+        System.out.println("hi");
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        int maze[][] = 
+            {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+             {0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+             {0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0},
+             {0,2,0,4,4,0,1,0,4,4,4,0,1,0,0,1,0,4,4,4,0,1,0,4,4,0,1,0},
+             {0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0},
+             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+             {0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0},
+             {0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0},
+             {0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0},
+             {0,0,0,0,0,0,1,0,0,0,0,0,3,0,0,3,0,0,0,0,0,1,0,0,0,0,0,0},
+             {4,4,4,4,4,0,1,0,0,0,0,0,3,0,0,3,0,0,0,0,0,1,0,4,4,4,4,4},
+             {4,4,4,4,4,0,1,0,0,3,3,3,3,3,3,3,3,3,3,0,0,1,0,4,4,4,4,4},
+             {4,4,4,4,4,0,1,0,0,3,0,0,0,4,4,0,0,0,3,0,0,1,0,4,4,4,4,4},
+             {0,0,0,0,0,0,1,0,0,3,0,4,4,4,4,4,4,0,3,0,0,1,0,0,0,0,0,0},
+             {3,3,3,3,3,3,1,3,3,3,0,4,4,4,4,4,4,0,3,3,3,1,3,3,3,3,3,3},
+             {0,0,0,0,0,0,1,0,0,3,0,4,4,4,4,4,4,0,3,0,0,1,0,0,0,0,0,0},
+             {4,4,4,4,4,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,4,4,4,4,4},
+             {4,4,4,4,4,0,1,0,0,3,3,3,3,3,3,3,3,3,3,0,0,1,0,4,4,4,4,4},
+             {4,4,4,4,4,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,4,4,4,4,4},
+             {0,0,0,0,0,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,0,0,0,0,0},
+             {0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+             {0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0},
+             {0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0},
+             {0,2,1,1,0,0,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,0,0,1,1,2,0},
+             {0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0},
+             {0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0},
+             {0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0},
+             {0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
+             {0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
+             {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+        //now i need to print the array
+        for(int i = 0; i < mazeRows; i++){
+            for(int j = 0; j<mazeColumns; j++){
+                System.out.print(maze[j][i]);
+                switch (maze[j][i]){
+                    case 0:
+                        g2.setColor(Color.BLACK);
+                        g2.fillRect(i*cellSize,j*cellSize, cellSize, cellSize);
+                        System.out.print("hi");
+                    case 1:
+                        g2.setColor(new Color(255,238,238));
+                        break;
+                }    
+                }
             }
     }
+            }
